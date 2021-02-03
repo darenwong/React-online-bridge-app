8149d9d4df0c744a0d8f06e83cd9c692d16fc2b7
Daren Wong <daren96123@gmail.com>
2021-02-03 21:43:39 +0800

    Commit to push db to heroku

--- server/db.js
+++ server/db.js
@@ -1,10 +1,10 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-    user: "postgres",
-    password: "abcde12345",
-    host: "localhost",
+    user: "ycerfxrqdubcii",
+    password: "339ccdeb8c5541562f66b233a8a3a4bad40586604bae17a114a8086ed0479126",
+    host: "ec2-34-224-254-126.compute-1.amazonaws.com",
     port: 5432,
-    database: "floatingbridge",
+    database: "ddbvnbpdt0921s",
 })
 module.exports= pool;
