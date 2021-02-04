883e04512c4750222523421b47c840a19d118f64
Daren Wong <daren96123@gmail.com>
2021-02-04 08:23:35 +0800

    set SSL rejectUnauthorized to false so that node can query db

--- server/db.js
+++ server/db.js
@@ -2,7 +2,7 @@ const Pool = require("pg").Pool;
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
-    ssl: true
+    ssl: { rejectUnauthorized: false }
     })
 pool.on('connect', () => console.log('connected to db'));
 
